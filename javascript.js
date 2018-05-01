dfd66d6a10a552f10967d6b5385027e713b458c6
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2018-05-01 17:16:29 +0200

    mobilfrem med zoom-start

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
-$(window).on("load", sideLoad)
+// - - - - - - - - sideLoad - - - - - - - -
+
+$(window).on("load", sideLoad);
 
 
 
@@ -12,6 +14,8 @@ function sideLoad() {
 	startBillede();
 }
 
+// - - - - - - - - Scene startBillede - - - - - - - -
+
 function startBillede() {
 	console.log("startBillede");
 
@@ -20,17 +24,19 @@ function startBillede() {
 }
 
 function startknapKlik() {
-	console.log("startknapKlik");
+	console.log("startknapKlik!");
 
 	// Start knaplyd effekt_bank
 	$("#game_start")[0].play();
 
-	//  Når lyden har spillet spillet
+	// gå til eleverInd efter tid
 	setTimeout(eleverInd, 500);
 }
 
+// - - - - - - - - Scene eleverInd - - - - - - - -
+
 function eleverInd() {
-	console.log("eleverInd");
+	console.log("eleverInd!");
 
 	//	Startbillede fades
 	$("#startbillede").addClass("fadeout");
@@ -44,22 +50,34 @@ function eleverInd() {
 
 	$("#anders_sprite").addClass("anders_walk_cycle");
 
+	//	klokkelyd
 	$("#school_bell")[0].play();
 
-	$("#brian_container").on("animationend", alle_seatter_sig);
+	//  Trigger - Når lyden Brian er ved sidde possition
+	$("#brian_container").on("animationend", alle_saetter_sig);
 
 }
 
-function alle_seatter_sig() {
-	console.log("alle_seatter_sig!")
+function alle_saetter_sig() {
+	console.log("alle_saetter_sig!")
+
+	$("#brian_container").off("animationend", alle_saetter_sig);
 
 	$("#startbillede").removeClass("vis");
 	$("#startbillede").addClass("skjul");
 
+	$("#anders_container").removeClass("anders_move_right");
+	$("#anders_container").addClass("anders_sidder_possition");
+
+	$("#brian_container").removeClass("brian_move_right");
+	$("#brian_container").addClass("brian_sidder_possition");
+
 	$("#anders_sprite").removeClass("anders_walk_cycle");
+	$("#anders_sprite").addClass("anders_sit");
+
 	$("#brian_sprite").removeClass("brian_stand");
-	$("#brian_container").off("animationend", alle_seatter_sig);
 	$("#brian_sprite").addClass("brian_sit");
+
 	setTimeout(lyd_mobil, 1500);
 }
 
@@ -67,4 +85,22 @@ function lyd_mobil() {
 	console.log("lyd_mobil!")
 	$("#mobil")[0].play();
 	$("#anders_container").addClass("anders_mobil_ryster");
+
+	//  Trigger - Når mobil har rystet
+
+	setTimeout(MobilFrem, 2000);
+}
+
+// - - - - - - - - Scene MobilFrem - - - - - - - -
+
+function MobilFrem() {
+	console.log("MobilFrem!")
+
+	$("#anders_container").off("animationend", MobilFrem);
+
+	$("#anders_sprite").removeClass("anders_sit");
+	$("#anders_sprite").addClass("anders_mobil");
+
+	$("#zoomframe").addClass("mobil_frem_zoom");
+	//	$("#baggrund").addClass("baggrund_blur");
 }
